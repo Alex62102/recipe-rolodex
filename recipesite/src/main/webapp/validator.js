7f86fd7b1fa393a4be2fe1b7c355f4c8113ec469
GarbanzoBoy <alphanerdling@cox.net>
2024-12-18 14:57:53 -0600

    Working with Validation
    Created the JavaScript validator and ran into issues getting it to talk with the JSF components

---
+++ recipesite/src/main/webapp/validator.js
@@ -0,0 +1,53 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+function validatePassChange() {
+    
+    var oldpass=document.forms["passchange"]["oldpass"].value;
+    alert(oldpass);
+    var newpass=document.getElementById(newpass).value;
+    var newpassconfirm=document.getElementById(newpassconfirm).value;
+    
+    //Update to check old password against user password
+    if(oldpass === null || oldpass === "") {
+        alert("Old Password cannot be left blank");
+        return false;
+    } else if(newpass === null || newpass === "") {
+        alert("New Password cannot be left blank");
+        return false;
+    } else if(badPassword(newpass)) {
+        alert("New password must be at least 8 characters and not contain whitespace");
+        return false;
+    } else if(newpass !== newpassconfirm) {
+        alert("New password confirmation does not match");
+        return false;
+    } else if(newpass === oldpass) {
+        alert("New password cannot be same as old password");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateLogin() {
+    
+}
+
+function validateRecipe() {
+    
+}
+
+function validateNewUser() {
+    
+}
+
+function badPassword(pass) {
+    if(pass === null || pass === "") return true;
+    if(typeof pass !== "string") return true;
+    if(pass.length < 8) return true;
+    if(/\s/g.test(pass)) return true;
+    return false;
+}
+
